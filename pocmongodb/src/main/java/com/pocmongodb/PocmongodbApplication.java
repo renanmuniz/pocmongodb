7ffb83618c9c81c274a12f463291c92f589270a6
renanmuniz <renan.merlo@paygo.com.br>
2022-09-19 15:27:12 -0300

    criacao do projeto

---
+++ pocmongodb/src/main/java/com/pocmongodb/PocmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.pocmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocmongodbApplication.class, args);
+	}
+
+}
